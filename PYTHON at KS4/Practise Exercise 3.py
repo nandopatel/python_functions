c1718809fb8b8d1ecbbeb717a74855ebb1ea4fa0
Anand <nandopatel2001@yahoo.co.uk>
2017-08-29 09:02:29 -0700

    Add files via upload

---
+++ PYTHON at KS4/Practise Exercise 3.py
@@ -0,0 +1,40 @@
+quote = input("Enter your favorite quote: ")
+
+name = input("Enter your name: ")
+
+num1 = input("Enter num1: ")
+num2 = input("Enter num2: ")
+num3 = input("Enter num3: ")
+print(int(num1)+int(num2)+int(num3))
+
+age = input("Enter age: ")
+name = input("Enter name: ")
+print("Hi {}, you are {}. :-)".format(name,age))
+
+title = "Python for key stage 4"
+print(title[11:14])
+
+name = "Benjamin"
+print("Hello {}, Python is fun".format(name))
+
+def double(n):print(n*2)
+
+def perimofrect(w,l):print((w*2)+(l*2))
+
+import math
+def areaofcircle(r):print(math.pi*r**2)
+
+def hellox3(n):
+  print(("hi",n)*3,)
+
+nn = str(input("Enter name: "))
+def head_name(name): 
+  print(name[:1])
+head_name(nn)
+
+nn = str(input("Enter name: "))
+def tail_name(name): 
+  print(name[1:])
+tail_name(nn)
+
+
---
+++ PYTHON at KS4/Practise Exercise 4.py
@@ -0,0 +1,23 @@
+import math
+print(60*60*24*30)
+
+daysinyear = 365
+print(60*60*24*daysinyear)
+
+print((414/150.0))
+
+print(500*1.20)
+
+print(320*0.68)
+
+radius = 10
+output = (4.0/3)*math.pi*radius**3
+print "Volume of sphere with radius {} = {}\n".format(radius,output)
+
+print(36/9-2)
+print(36.0/(9-2))
+
+def power_value(base,power):
+	ans = base**power
+	print("{} to the power of {} is {}".format(base,power,ans))
+	
