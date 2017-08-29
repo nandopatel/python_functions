f08aad1420d710b0a25af254e7f4736fa4625ed4
Anand <nandopatel2001@yahoo.co.uk>
2017-08-29 09:00:59 -0700

    Add files via upload
    Working through my 2017 summer booklet

---
+++ PYTHON at KS4/Practise Exercise 1.py
@@ -0,0 +1,19 @@
+import math
+print(60*60*24*30)
+
+daysinyear = 365
+print(60*60*24*daysinyear)
+
+print((414/150.0))
+
+print(500*1.20)
+
+print(320*0.68)
+
+radius = 10
+output = (4.0/3)*math.pi*radius**3
+print "Volume of sphere with radius {} = {}\n".format(radius,output)
+
+print(36/9-2)
+print(36.0/(9-2))
+
