f81815a1eb393657b7f1292cfe52d121199b9943
Anand <nandopatel2001@yahoo.co.uk>
2017-08-29 09:01:40 -0700

    Add files via upload

---
+++ PYTHON at KS4/Practise Exercise 2.py
@@ -0,0 +1,30 @@
+l = 18
+w = 7
+print("Area = {}, Perimeter = {}").format(l*w,2*l+2*w)
+
+days_in_school_year = 192
+print("hours in school from y7 to 11 = {}").format(192*6*4)
+
+marks = 25
+marks +=10
+print(marks)
+
+import math
+print(60*60*24*30)
+
+daysinyear = 365
+print(60*60*24*daysinyear)
+
+print((414/150.0))
+
+print(500*1.20)
+
+print(320*0.68)
+
+radius = 10
+output = (4.0/3)*math.pi*radius**3
+print "Volume of sphere with radius {} = {}\n".format(radius,output)
+
+print(36/9-2)
+print(36.0/(9-2))
+
