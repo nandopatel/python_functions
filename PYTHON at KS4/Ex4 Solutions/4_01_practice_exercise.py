0803e9f61b292a977024621879eb4370646b1117
Anand Patel <nandopatel2001@yahoo.co.uk>
2017-09-22 23:58:29 +0100

    Add files via upload

---
+++ PYTHON at KS4/Ex4 Solutions/4_01_practice_exercise.py
@@ -0,0 +1,22 @@
+#1) Define a function called double, that takes a number and return the number doubled
+def double(num):
+    return num*2
+
+number = int(input("Please enter a number to double: "))
+print(number, "doubled is ", double(number))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
