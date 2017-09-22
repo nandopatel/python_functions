67540de11b7c25b9068e115eced72d5e70235efc
Anand Patel <nandopatel2001@yahoo.co.uk>
2017-09-23 00:04:23 +0100

    Add files via upload

---
+++ PYTHON at KS4/Ex4 Solutions/4_08_practice_exercise.py
@@ -0,0 +1,3 @@
+#8) What is the difference between parameter and arguments?
+#parameters are place holders that are used inside the function, whereas arguments
+#are the actual values that you send when calling the funtion.
