3f028bea50da66cbec0c2c116b6f92008c491499
Anand Patel <nandopatel2001@yahoo.co.uk>
2017-09-22 23:55:49 +0100

    Add files via upload

---
+++ PYTHON at KS4/Ex2 Solutions/2_practice_exercise.py
@@ -0,0 +1,63 @@
+#1) Write a program that assigns the variables length and width to be 18 and 7
+#respectively.  Use the variables to calculate the perimeter and area
+#of the rectangle. 
+length = 18
+width = 7
+perimeter = (2*length) + (2*width)
+print(perimeter)
+
+#2) Write a python program that defines a variable called days_in_each_school_year
+#and assign 192 to the variable.  The program should then print out the total hours
+#that you spend in school from year 7 to year 11, assuming that each day you spend
+#6 hours in school
+days_in_each_school_year = 192
+print(5*days_in_each_school_year * 6)
+
+
+#3) What value will be stored in marks:
+marks = 25
+marks = marks + 10
+print(marks)
+
+
+
+#4. Given the code below, what value will be printed to the screen? 
+time_spent = 34 # in minutes
+# after one minutes
+time_spent = time_spent +1
+print(time_spent)
+
+#5. Which of the value below would be printed on the screen from the code snippet?
+#hours_in_a_week = hours_in_a_day  * 7
+#hours_in_a_month = hours_in_a_week * 30 # assuming we have 30 days in a month
+#print (hours_in_a_month)
+
+#d.	Error   --> hours_in_a_day is not defined and therefore will generate an error.
+
+#6) What is the value of score after running the following code?
+score = 24
+number_of_pieces = 2
+new_score = score *2
+print(score)
+
+#7) False:  An expression can be assigned to a variable.
+#--> A variable can be assigned an expression, not the other way around.
+
+#What is the value of y after running the code?
+x, y = 23, 45
+y, x = x, y
+print(y)
+
+#8) Which of the following are not valid python variable name?
+#4gs	&item	_age		
+
+#9) How do we use the help() function in python?
+#by typing help() at the prompt
+
+
+#10)Which of the following statements will assign the value 365 to the
+#variable days_in_a_year ?
+
+#c.	days in a year = 365
+
+
