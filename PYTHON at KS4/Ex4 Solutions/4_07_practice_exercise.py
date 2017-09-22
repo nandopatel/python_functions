83bb1059f22ca8604205856b6028ba837ac7d545
Anand Patel <nandopatel2001@yahoo.co.uk>
2017-09-23 00:03:59 +0100

    Add files via upload

---
+++ PYTHON at KS4/Ex4 Solutions/4_07_practice_exercise.py
@@ -0,0 +1,19 @@
+#7) Give three advantages of using functions in a program
+
+#Using functions make our program code more readable.
+#When we define a function we are actually naming certain
+#set of instruction that are performing a specific task and
+#therefore making our program code easier to read. 
+
+
+#Functions can reduce our program code significantly by putting
+#repetitive code into a function. Change or adjustments can be made in one place.
+
+
+#Functions facilitate modular design.
+#We can break down a large problem into manageable chunks,
+#work on these chunks in functions then put the entire working piece
+#together for a working solution.
+
+
+#Function can be placed into modules so that they can be reused. 
