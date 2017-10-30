ca17ae2e0f4bb115ddf4dbaf0e49e5100b3fa452
Anand Patel <nandopatel2001@yahoo.co.uk>
2017-10-30 23:31:24 +0000

    valid response gist
    for school task - school is so boring

---
+++ w1t1.py
@@ -0,0 +1,8 @@
+def yorn():
+    userresp = raw_input("answer y or n: ")
+    if userresp == 'y':
+        return True
+    elif userresp == 'n':
+        return True
+    else:
+        return False
